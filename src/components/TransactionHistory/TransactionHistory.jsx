4f234cd03047a3a01e8637b88e61d2bd84f4a88d
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-07-25 22:10:14 +0300

    +ultra

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,33 +1,33 @@
-import TransactionHistoryItem from "./TransactionHistoryItem";
+// import TransactionHistoryItem from "./TransactionHistoryItem";
 
-const TransactionHistory = (props) => {
-  const {transactions}= props;
-  return (
-    <table className="transaction-history" style={{
-      height: '100px',
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: 20,
-      color: '#010101',
-    }}>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-       {transactions.map((el)=>(
-      <TransactionHistoryItem
-      key={el.id}
-      {...el}/>
-       ))}
-      </tbody>
-    </table>
-  );
-};
+// const TransactionHistory = (props) => {
+//   const {transactions}= props;
+//   return (
+//     <table className="transaction-history" style={{
+//       height: '100px',
+//       display: 'flex',
+//       flexWrap: 'wrap',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       fontSize: 20,
+//       color: '#010101',
+//     }}>
+//       <thead>
+//         <tr>
+//           <th>Type</th>
+//           <th>Amount</th>
+//           <th>Currency</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//        {transactions.map((el)=>(
+//       <TransactionHistoryItem
+//       key={el.id}
+//       {...el}/>
+//        ))}
+//       </tbody>
+//     </table>
+//   );
+// };
 
-export default TransactionHistory;
+// export default TransactionHistory;
--- src/components/TransactionHistory/TransactionHistoryItem.jsx
+++ src/components/TransactionHistory/TransactionHistoryItem.jsx
@@ -1,12 +1,12 @@
-const TransactionHistoryItem = ({ type, amount, currency }) => {
-    console.log(amount);
-  return (
-    <tr>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
-    </tr>
-  );
-};
+// const TransactionHistoryItem = ({ type, amount, currency }) => {
+//     console.log(amount);
+//   return (
+//     <tr>
+//       <td>{type}</td>
+//       <td>{amount}</td>
+//       <td>{currency}</td>
+//     </tr>
+//   );
+// };
 
-export default TransactionHistoryItem;
+// export default TransactionHistoryItem;
